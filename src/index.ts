f36042372eeddabb699f8e9e5455fc6e4d8df6da
范锐平 <85617901@qq.com>
2019-04-08 18:21:55 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,25 @@ function importAlias(alias: IAlias = {}): any {
       return;
     }
     const aliasNames = Object.keys(alias);
-    if (aliasNames.length > 0) {
-      const aliasStr = aliasNames.join('|');
-      const importReg = new RegExp(`import\\s*\\{?\\s*.*\\s*\\}?(?:\\s*from\\s*)?['"](${aliasStr})(?:\\/[\\w_.-]+)*['"]`, 'ig');
-
-      let contents = file.contents.toString();
-      const cwd = file.cwd;
-      contents = contents.replace(importReg, (m, key) => {
-        const aliasPath = path.resolve(cwd, alias[key]);
-        let aliasRelative = path.relative(path.dirname(file.path), aliasPath);
-        if (path.sep === '\\') {
-          aliasRelative = aliasRelative.split(path.sep).join('/');
-        }
-        aliasRelative = /^\./.test(aliasRelative) ? aliasRelative : `./${aliasRelative}`;
-        return m.replace(m, m.replace(key as string, aliasRelative));
-      });
-      file.contents = Buffer.from(contents);
+    if (!aliasNames.length) {
+      callback(undefined, file);
+      return;
     }
+
+    const importReg = new RegExp(`import\\s*\\{?\\s*.*\\s*\\}?(?:\\s*from\\s*)?['"](${aliasNames.join('|')})(?:\\/[\\w_.-]+)*['"]`, 'ig');
+
+    let contents = file.contents.toString();
+    const cwd = file.cwd;
+    contents = contents.replace(importReg, (m, key) => {
+      const aliasPath = path.resolve(cwd, alias[key]); // 路径
+      let aliasRelative = path.relative(path.dirname(file.path), aliasPath); // 相对路径
+      if (path.sep === '\\') {
+        aliasRelative = aliasRelative.split(path.sep).join('/');
+      }
+      aliasRelative = /^\./.test(aliasRelative) ? aliasRelative : `./${aliasRelative}`;
+      return m.replace(m, m.replace(key as string, aliasRelative));
+    });
+    file.contents = Buffer.from(contents);
 
     callback(undefined, file);
   });
